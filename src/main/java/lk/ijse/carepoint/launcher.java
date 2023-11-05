d341d730c863a9cb0a55bde3fd75f63a64e57c0d
UvinduV <uvindusanketh470@gmail.com>
2023-11-05 23:05:13 +0530

    create controller pages

--- src/main/java/lk/ijse/carepoint/launcher.java
+++ src/main/java/lk/ijse/carepoint/launcher.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(launcher.class.getResource("/view/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(launcher.class.getResource("/view/login_form.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
